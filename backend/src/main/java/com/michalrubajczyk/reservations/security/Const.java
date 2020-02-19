2f222ce333be06853913669a2c6a12ac54d2e327
Michal Rubajczyk <rubajticos@gmail.com>
2020-02-19 18:34:18 +0100

    Impement AuthorizationServer config

---
+++ backend/src/main/java/com/michalrubajczyk/reservations/security/Const.java
@@ -0,0 +1,7 @@
+package com.michalrubajczyk.reservations.security;
+
+public class Const {
+
+    public static final String CLIENT_ID = "my-client";
+    public static final String CLIENT_SECRET = "{noop}my-secret";
+}
