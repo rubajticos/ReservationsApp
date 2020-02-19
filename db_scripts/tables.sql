5bf71cc74d4b3e0bbe0d74e99d5fcddd11c9d524
Michal Rubajczyk <rubajticos@gmail.com>
2020-02-19 21:35:51 +0100

    Store db scripts needed

---
+++ db_scripts/tables.sql
@@ -0,0 +1,16 @@
+-- Create tables to store oAuth2 tokens in database
+CREATE TABLE oauth_access_token (
+    token_id VARCHAR(255),
+    token BLOB,
+    authentication_id VARCHAR(255) PRIMARY KEY,
+    user_name VARCHAR(255),
+    client_id VARCHAR(255),
+    authentication BLOB,
+    refresh_token VARCHAR(255)
+);
+
+CREATE TABLE oauth_refresh_token (
+    token_id VARCHAR(255),
+    token BLOB,
+    authentication BLOB
+);
