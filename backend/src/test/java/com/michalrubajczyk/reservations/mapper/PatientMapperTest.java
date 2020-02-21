1270941f66e1fdb268be849dbf6ad6214140bfb9
Michal Rubajczyk <rubajticos@gmail.com>
2020-02-21 11:24:46 +0100

    Test PatientMapper

--- backend/src/test/java/com/michalrubajczyk/reservations/mapper/PatientMapperTest.java
+++ backend/src/test/java/com/michalrubajczyk/reservations/mapper/PatientMapperTest.java
@@ -1,6 +1,8 @@
 package com.michalrubajczyk.reservations.mapper;
 
-import org.junit.jupiter.api.AfterEach;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -11,30 +13,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PatientMapperTest {
     private static PatientMapper patientMapper;
-    private Patient patient;
-    private PatientDTO patientDTO;
 
     @BeforeAll
     static void setUp() {
         patientMapper = new PatientMapper();
     }
 
-    @AfterEach
-    void tearDown() {
-        this.patient = null;
-        this.patientDTO = null;
+    @Test
+    void patientShouldBeConvertedToPatientDTO() {
+        Patient patient = generatePatient();
+
+        PatientDTO patientDTO = patientMapper.entityToDto(patient);
+
+        assertPatientDTO(patientDTO, patient);
     }
 
     @Test
-    void patientShouldBeConvertedToPatientDTO() {
-        generatePatient();
+    void patientsListShouldBeConvertedToPatientsDTOList() {
+        List<Patient> patients = generatePatientList();
 
-        this.patientDTO = patientMapper.entityToDto(this.patient);
+        List<PatientDTO> patientDTOList = patientMapper.entityListToDtoList(patients);
 
-        assertPatientDTO();
+        for (int i = 0; i < patientDTOList.size(); i++) {
+            assertPatientDTO(patientDTOList.get(i), patients.get(i));
+        }
     }
 
-    private void assertPatientDTO() {
+    private void assertPatientDTO(PatientDTO patientDTO, Patient patient) {
         assertEquals(patientDTO.getId(), patient.getId());
         assertEquals(patientDTO.getUserName(), patient.getFirstName());
         assertEquals(patientDTO.getLastName(), patient.getLastName());
@@ -43,35 +48,77 @@ class PatientMapperTest {
 
     @Test
     void patientDTOShouldBeConvertedToPatient() {
-        generatePatientDTO();
+        PatientDTO patientDTO = generatePatientDTO();
 
-        this.patient = patientMapper.dtoToEntity(this.patientDTO);
+        Patient patient = patientMapper.dtoToEntity(patientDTO);
 
-        assertPatient();
+        assertPatient(patient, patientDTO);
     }
 
-    private void assertPatient() {
+    @Test
+    void patientsDTOListShouldBeConvertedToPatientsList() {
+        List<PatientDTO> patientsDTO = generatePatientDTOList();
+
+        List<Patient> patients = patientMapper.dtoListToEntityList(patientsDTO);
+
+        for (int i = 0; i < patients.size(); i++) {
+            assertPatient(patients.get(i), patientsDTO.get(i));
+        }
+    }
+
+    private void assertPatient(Patient patient, PatientDTO patientDTO) {
         assertEquals(patient.getId(), patientDTO.getId());
         assertEquals(patient.getFirstName(), patientDTO.getUserName());
         assertEquals(patient.getLastName(), patientDTO.getLastName());
         assertEquals(patient.getPhoneNumber(), patientDTO.getPhoneNumber());
     }
 
-    private void generatePatient() {
-        this.patient = new Patient();
+    private Patient generatePatient() {
+        Patient patient = new Patient();
         patient.setId(1L);
         patient.setFirstName("a");
         patient.setLastName("b");
         patient.setPhoneNumber("123456789");
+
+        return patient;
     }
 
-    private void generatePatientDTO() {
-        this.patientDTO = new PatientDTO();
+    private PatientDTO generatePatientDTO() {
+        PatientDTO patientDTO = new PatientDTO();
         patientDTO.setId(1L);
         patientDTO.setUserName("a");
         patientDTO.setLastName("b");
         patientDTO.setPhoneNumber("123456789");
+
+        return patientDTO;
     }
 
+    private List<Patient> generatePatientList() {
+        List<Patient> patients = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Patient p = new Patient();
+            p.setId((long) i);
+            p.setFirstName("a");
+            p.setLastName("b");
+            p.setPhoneNumber("123456789");
+            patients.add(p);
+        }
+
+        return patients;
+    }
+
+    private List<PatientDTO> generatePatientDTOList() {
+        List<PatientDTO> patientsDTO = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            PatientDTO p = new PatientDTO();
+            p.setId((long) i);
+            p.setUserName("a");
+            p.setLastName("b");
+            p.setPhoneNumber("123456789");
+            patientsDTO.add(p);
+        }
+
+        return patientsDTO;
+    }
 
 }
