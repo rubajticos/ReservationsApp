9076d005364df89ef783f0755b97f0e82fa5171c
Michal Rubajczyk <rubajticos@gmail.com>
2020-02-26 22:31:39 +0100

    Create navigation header component

--- frontend/my-reservations-client/src/app/app.module.ts
+++ frontend/my-reservations-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavheaderComponent } from './components/navheader/navheader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavheaderComponent
   ],
   imports: [
     BrowserModule,
