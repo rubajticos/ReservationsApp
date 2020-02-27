1b5c3db53b6eecc2b171a5b60ede4471c08d622d
Michal Rubajczyk <rubajticos@gmail.com>
2020-02-27 21:32:45 +0100

    Create 404 page and register stubs

--- frontend/my-reservations-client/src/app/app.module.ts
+++ frontend/my-reservations-client/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavheaderComponent } from './components/navheader/navheader.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavheaderComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
