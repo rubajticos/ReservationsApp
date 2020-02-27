220619ac295bc3deb4a1fe4c2719dba3f83e4808
Michal Rubajczyk <rubajticos@gmail.com>
2020-02-27 20:27:56 +0100

    Create footer and main layout

--- frontend/my-reservations-client/src/app/app.module.ts
+++ frontend/my-reservations-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavheaderComponent } from './components/navheader/navheader.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavheaderComponent
+    NavheaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
