283e533b09fc01ffa1e3df2fe68f97a98cbe54e4
Michal Rubajczyk <rubajticos@gmail.com>
2020-03-12 21:27:03 +0100

    Prepare register form with databinding

--- frontend/my-reservations-client/src/app/app.module.ts
+++ frontend/my-reservations-client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { NavheaderComponent } from './components/navheader/navheader.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
@@ -18,7 +19,8 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/my-reservations-client/src/app/components/register/register.component.ts
+++ frontend/my-reservations-client/src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Patient } from 'src/app/model/patient';
+import { UserRegister } from 'src/app/model/user-register';
+import { RegisterModel } from 'src/app/model/register-model';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  model = new RegisterModel(new UserRegister(), new Patient());
+
+  submitted = false;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  onSubmit() { this.submitted = true; }
+
+  get diagnostic() { return JSON.stringify(this.model); }
 
 }
