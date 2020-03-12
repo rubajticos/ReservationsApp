9dfd18f5613f8683c43ffa94c06bd16fb5007f5f
Michal Rubajczyk <rubajticos@gmail.com>
2020-03-12 20:51:09 +0100

    Create Patient model

---
+++ frontend/my-reservations-client/src/app/model/patient.ts
@@ -0,0 +1,11 @@
+export class Patient {
+
+  constructor(
+    public id: BigInteger,
+    public firstName: string,
+    public lastName: string,
+    public phone: string,
+    public email: string,
+  ) { }
+
+}
