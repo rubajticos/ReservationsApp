028047cccad293ab8df4fad6411ffa0c6a4fe68b
Michal Rubajczyk <rubajticos@gmail.com>
2020-03-14 16:00:35 +0100

    Implement registation method

--- frontend/my-reservations-client/src/app/services/register-service.service.ts
+++ frontend/my-reservations-client/src/app/services/register-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { RegisterModel } from '../model/register-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterServiceService {
 
-  constructor() { }
+  readonly registerURL = environment.apiBaseUrl + 'register';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  public registerPatient(patient: RegisterModel) {
+    this.http.post(this.registerURL, patient)
+      .subscribe(response => {
+        console.log(response.toString);
+      });
+  }
+
+
+
 }
