5e24264939d26cb276ecd168aae9b1ba35ed36ff
Michal Rubajczyk <rubajticos@gmail.com>
2020-03-16 21:47:00 +0100

    Display confirmation message after submit

--- frontend/my-reservations-client/src/app/model/patient.ts
+++ frontend/my-reservations-client/src/app/model/patient.ts
@@ -3,7 +3,7 @@ export class Patient {
   public id: BigInteger;
   public firstName: string;
   public lastName: string;
-  public phone: string;
+  public phoneNumber: string;
   public email: string;
 
   constructor() { }
