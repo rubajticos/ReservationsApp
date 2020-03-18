7467a201960e4465b11c0c82df4235f0e9cbf785
Michal Rubajczyk <rubajticos@gmail.com>
2020-03-18 20:08:04 +0100

    Configure routing for login

--- frontend/my-reservations-client/src/app/app-routing.module.ts
+++ frontend/my-reservations-client/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './components/register/register.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const appRoutes: Routes = [
   { path: 'signup', component: RegisterComponent },
   { path: '', component: RegisterComponent },
+  { path: 'login', component: LoginComponent},
   { path: '**', component: PageNotFoundComponent }
 ];
 
