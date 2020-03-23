c9c90ed61d92b7b82751034bb15ebad3dafc4db6
Michal Rubajczyk <rubajticos@gmail.com>
2020-03-23 22:12:06 +0100

    Implement countdown timer to redirect after login success

--- frontend/my-reservations-client/src/app/components/login/login.component.ts
+++ frontend/my-reservations-client/src/app/components/login/login.component.ts
@@ -4,6 +4,7 @@ import { HttpErrorResponse, HttpResponse, HttpEvent } from '@angular/common/http
 import { CookieService } from 'ngx-cookie-service';
 import { AuthorizationModel } from 'src/app/model/authorization-model';
 import { Router } from '@angular/router';
+import { timer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -20,6 +21,7 @@ export class LoginComponent implements OnInit {
   errorMessage: string;
   loading = false;
   seconds = 5;
+  countdownTimer: Subscription;
 
   constructor(private loginService: LoginService, private cookieService: CookieService, private router: Router) { }
 
@@ -35,6 +37,7 @@ export class LoginComponent implements OnInit {
           this.loading = false;
           this.displaySuccess = true;
           this.saveTokens(data);
+          this.startRedirectCountdown();
         },
         (error: HttpErrorResponse) => {
           this.loading = false;
@@ -54,7 +57,21 @@ export class LoginComponent implements OnInit {
     this.cookieService.set('refresh_token', authModel.refresh_token);
   }
 
-  public redirectToVisits() {
+  private startRedirectCountdown(): void {
+    this.countdownTimer = timer(1000, 1000).subscribe(val => {
+      this.performCountdownAction();
+    });
+  }
+
+  private performCountdownAction() {
+    this.seconds--;
+    if (this.seconds === 0) {
+      this.redirectToHome();
+      this.countdownTimer.unsubscribe();
+    }
+  }
+
+  public redirectToHome() {
     this.router.navigate(['/']);
   }
 
