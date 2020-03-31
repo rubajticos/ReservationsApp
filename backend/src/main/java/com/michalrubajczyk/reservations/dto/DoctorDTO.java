01fe224a198a95b818cbb169e43f8896bec5645a
Michal Rubajczyk <rubajticos@gmail.com>
2020-03-31 20:47:08 +0200

    Create DTO for Docotr

---
+++ backend/src/main/java/com/michalrubajczyk/reservations/dto/DoctorDTO.java
@@ -0,0 +1,12 @@
+package com.michalrubajczyk.reservations.dto;
+
+import lombok.Data;
+
+@Data
+public class DoctorDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String specialization;
+}
