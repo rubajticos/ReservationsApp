d9d3d8d6d484d7499f7fccd489f9b20ef5fa8f35
Michal Rubajczyk <rubajticos@gmail.com>
2020-04-07 20:36:22 +0200

    Extend repository to get visits for patient

--- backend/src/main/java/com/michalrubajczyk/reservations/repository/VisitRepository.java
+++ backend/src/main/java/com/michalrubajczyk/reservations/repository/VisitRepository.java
@@ -1,9 +1,14 @@
 package com.michalrubajczyk.reservations.repository;
 
+import com.michalrubajczyk.reservations.entity.Patient;
 import com.michalrubajczyk.reservations.entity.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface VisitRepository extends JpaRepository<Visit, Long> {
+
+    Set<Visit> findByPatient(Patient patient);
 }
