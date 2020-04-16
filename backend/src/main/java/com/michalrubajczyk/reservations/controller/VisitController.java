c46a7eb587a185b8c8d88e6cd3f47ff0e8750853
Michal Rubajczyk <rubajticos@gmail.com>
2020-04-16 21:27:56 +0200

    Create endpoint for get all visits for patient

--- backend/src/main/java/com/michalrubajczyk/reservations/controller/VisitController.java
+++ backend/src/main/java/com/michalrubajczyk/reservations/controller/VisitController.java
@@ -1,15 +1,18 @@
 package com.michalrubajczyk.reservations.controller;
 
 import com.michalrubajczyk.reservations.dto.VisitCreationDTO;
+import com.michalrubajczyk.reservations.dto.VisitDTO;
 import com.michalrubajczyk.reservations.service.VisitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @RestController
 public class VisitController {
@@ -28,5 +31,15 @@ public class VisitController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .build();
+    }
+
+    @GetMapping("visit/patient={patientId}")
+    public ResponseEntity getVisitsForPatient(@NotNull @PathVariable Long patientId, Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Set<VisitDTO> visitsForPatient = visitService.getVisitsForPatient(patientId, userDetails);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(visitsForPatient);
     }
 }
