4e1b9cc823621bb8d67ddfa6d14c890162ae4f8c
Michal Rubajczyk <rubajticos@gmail.com>
2020-05-01 13:37:03 +0200

    Replace alerts implementation to AlertInfo in Register form

--- frontend/my-reservations-client/src/app/components/register/register.component.ts
+++ frontend/my-reservations-client/src/app/components/register/register.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Patient } from 'src/app/model/patient';
 import { UserRegister } from 'src/app/model/user-register';
 import { RegisterModel } from 'src/app/model/register-model';
 import { RegisterService } from 'src/app/services/register.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { AlertInfoComponent } from '../alertinfo/alertinfo.component';
 
 @Component({
   selector: 'app-register',
@@ -11,16 +12,16 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild(AlertInfoComponent, { static: false }) alertInfo;
 
   phoneNumberPattern = '\\d+';
   model = new RegisterModel(new UserRegister(), new Patient());
   repassword: string;
 
   submitted = false;
-  registerSuccess = false;
-  registerError = false;
-  error: string;
   loading = false;
+  hideForm = false;
+  displayRegisteredData = false;
 
   constructor(private registerService: RegisterService) { }
 
@@ -28,23 +29,28 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit() {
+    this.alertInfo.clear();
     this.submitted = true;
     this.loading = true;
     this.registerService.registerPatient(this.model)
       .subscribe(success => {
         this.loading = false;
-        this.registerSuccess = true;
+        this.hideForm = true;
+        this.showRegisterSuccess();
       },
         (error: HttpErrorResponse) => {
           this.loading = false;
-          this.registerError = true;
-          this.error = error.error.message;
+          this.showRegisterError(error.error.message);
         });
   }
 
-  private resetAlerts() {
-    this.registerSuccess = false;
-    this.registerError = false;
+  private showRegisterSuccess() {
+    this.alertInfo.showSuccess('Success! You are registered succesfully!');
+    this.displayRegisteredData = true;
+  }
+
+  private showRegisterError(error: string) {
+    this.alertInfo.showError(error);
   }
 
 }
