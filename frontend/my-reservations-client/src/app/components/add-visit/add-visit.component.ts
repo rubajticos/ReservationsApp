a88f79b0eb60b65805fd8530a3a7362088ec1c40
Michal Rubajczyk <rubajticos@gmail.com>
2020-07-16 22:36:58 +0200

    Add form field for visit time

--- frontend/my-reservations-client/src/app/components/add-visit/add-visit.component.ts
+++ frontend/my-reservations-client/src/app/components/add-visit/add-visit.component.ts
@@ -13,6 +13,7 @@ export class AddVisitComponent implements OnInit {
 
   doctor: string;
   date: string;
+  time: string;
   doctors: Array<Doctor>;
 
   constructor(public dialogRef: MatDialogRef<AddVisitComponent>, private doctorsProvider: DoctorService) { }
@@ -27,7 +28,8 @@ export class AddVisitComponent implements OnInit {
     this.dialogRef.close(
       {
         doctor: this.doctor,
-        date: this.date
+        date: this.date,
+        time: this.time
       }
     );
   }
@@ -38,6 +40,12 @@ export class AddVisitComponent implements OnInit {
 
   public getDoctorDisplayName(doctor: Doctor) {
     return doctor.firstName + ' ' + doctor.lastName + ' - ' + doctor.specialization.name;
+  }
+
+  public getAvailableHours(): string[] {
+    return ['08:00', '08:30', '09:00', '09:30', '10:00', '10:30',
+      '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00',
+      '14:30', '15:00', '15:30'];
   }
 
 }
