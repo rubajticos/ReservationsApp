825307459e2ba1ffd41b25f59144bafeb77b4253
Michal Rubajczyk <rubajticos@gmail.com>
2021-01-21 09:53:47 +0100

    Fix tablename(user is reserved in postgres) and adjust initial data queries to new dialect

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,26 +3,26 @@
 -- Create tables to store oAuth2 tokens in database
 CREATE TABLE IF NOT EXISTS oauth_access_token (
     token_id VARCHAR(255),
-    token BLOB,
+    token BYTEA,
     authentication_id VARCHAR(255) PRIMARY KEY,
     user_name VARCHAR(255),
     client_id VARCHAR(255),
-    authentication BLOB,
+    authentication BYTEA,
     refresh_token VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS oauth_refresh_token (
     token_id VARCHAR(255),
-    token BLOB,
-    authentication BLOB
+    token BYTEA,
+    authentication BYTEA
 );
 
 --authority
-insert ignore into authority(id, name) values (1, 'ROLE_USER');
-insert ignore into authority(id, name) values (2, 'ROLE_ADMIN');
-insert ignore into authority(id, name) values (3, 'ROLE_GUEST');
+insert into authority(id, name) values (1, 'ROLE_USER') ON CONFLICT DO NOTHING;
+insert into authority(id, name) values (2, 'ROLE_ADMIN') ON CONFLICT DO NOTHING;
+insert into authority(id, name) values (3, 'ROLE_GUEST') ON CONFLICT DO NOTHING;
 
 --specialization
-insert ignore into specialization(id, name, type) values (1, 'Pediatrician', 'PEDIATRICIAN');
-insert ignore into specialization(id, name, type) values (2, 'Surgeon', 'SURGEON');
+insert into specialization(id, name, type) values (1, 'Pediatrician', 'PEDIATRICIAN') ON CONFLICT DO NOTHING;
+insert into specialization(id, name, type) values (2, 'Surgeon', 'SURGEON') ON CONFLICT DO NOTHING;
 
