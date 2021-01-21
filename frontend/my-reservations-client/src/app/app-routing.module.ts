4f8579a1f4e31adbe066d58038ec9d41d27f0a2a
Michal Rubajczyk <rubajticos@gmail.com>
2021-01-21 22:19:14 +0100

    Add AuthGuard

--- frontend/my-reservations-client/src/app/app-routing.module.ts
+++ frontend/my-reservations-client/src/app/app-routing.module.ts
@@ -1,22 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { RegisterComponent } from './components/register/register.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { LoginComponent } from './components/login/login.component';
-import { VisitComponent } from './components/visit/visit.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RegisterComponent } from "./components/register/register.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { LoginComponent } from "./components/login/login.component";
+import { VisitComponent } from "./components/visit/visit.component";
+import { AuthGuard } from "./auth-guard";
 
 const appRoutes: Routes = [
-  { path: 'signup', component: RegisterComponent },
-  { path: '', component: RegisterComponent },
-  { path: 'login', component: LoginComponent},
-  { path: 'visits', component: VisitComponent},
-  { path: '**', component: PageNotFoundComponent }
-
+  { path: "signup", component: RegisterComponent },
+  { path: "", component: VisitComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent },
+  { path: "visits", component: VisitComponent, canActivate: [AuthGuard] },
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
