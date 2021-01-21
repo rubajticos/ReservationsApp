5ad3328ee1261c53d6585ba07c6042828e95bce5
Michal Rubajczyk <rubajticos@gmail.com>
2021-01-21 23:20:29 +0100

    Remove unused LoginService

--- frontend/my-reservations-client/src/app/app.module.ts
+++ frontend/my-reservations-client/src/app/app.module.ts
@@ -1,46 +1,45 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { NavheaderComponent } from "./components/navheader/navheader.component";
-import { FooterComponent } from "./components/footer/footer.component";
-import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
-import { RegisterComponent } from "./components/register/register.component";
-import { MustMatchDirective } from "./directives/must-match.directive";
-import { RegisterService } from "./services/register.service";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { LoginComponent } from "./components/login/login.component";
-import { LoginService } from "./services/login.service";
-import { CookieService } from "ngx-cookie-service";
-import { AuthenticationService } from "./auth/authentication.service";
-import { TokenInterceptor } from "./interceptors/token-interceptor";
-import { VisitComponent } from "./components/visit/visit.component";
-import { PatientService } from "./services/patient.service";
-import { HttpVisitService } from "./services/http/http-visit.service";
-import { VisitService } from "./services/visit.service";
-import { AlertInfoComponent } from "./components/alertinfo/alertinfo.component";
-import { MatDialogModule } from "@angular/material/dialog";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AddVisitComponent } from "./components/add-visit/add-visit.component";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatSelectModule } from "@angular/material/select";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MAT_DATE_LOCALE } from "@angular/material/core";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavheaderComponent } from './components/navheader/navheader.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MustMatchDirective } from './directives/must-match.directive';
+import { RegisterService } from './services/register.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthenticationService } from './auth/authentication.service';
+import { TokenInterceptor } from './interceptors/token-interceptor';
+import { VisitComponent } from './components/visit/visit.component';
+import { PatientService } from './services/patient.service';
+import { HttpVisitService } from './services/http/http-visit.service';
+import { VisitService } from './services/visit.service';
+import { AlertInfoComponent } from './components/alertinfo/alertinfo.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AddVisitComponent } from './components/add-visit/add-visit.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 import {
   MatMomentDateModule,
   MAT_MOMENT_DATE_ADAPTER_OPTIONS,
-} from "@angular/material-moment-adapter";
-import { MatInputModule } from "@angular/material/input";
-import { DoctorService } from "./services/doctor.service";
+} from '@angular/material-moment-adapter';
+import { MatInputModule } from '@angular/material/input';
+import { DoctorService } from './services/doctor.service';
 import {
   NgxMatDatetimePickerModule,
   NgxMatNativeDateModule,
-} from "@angular-material-components/datetime-picker";
-import { DatePipe } from "@angular/common";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+} from '@angular-material-components/datetime-picker';
+import { DatePipe } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -74,7 +73,6 @@ import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
   ],
   providers: [
     RegisterService,
-    LoginService,
     CookieService,
     AuthenticationService,
     PatientService,
@@ -84,7 +82,7 @@ import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
     DatePipe,
     MatSnackBar,
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
-    { provide: MAT_DATE_LOCALE, useValue: "pl-PL" },
+    { provide: MAT_DATE_LOCALE, useValue: 'pl-PL' },
     { provide: MAT_MOMENT_DATE_ADAPTER_OPTIONS, useValue: { useUtc: true } },
   ],
   bootstrap: [AppComponent],
