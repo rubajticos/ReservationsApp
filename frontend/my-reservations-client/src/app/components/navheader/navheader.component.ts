521d1b9541a1fd8d24d979da731db95e0d9b1e00
Michal Rubajczyk <rubajticos@gmail.com>
2021-01-21 23:44:39 +0100

    Hide elements on header depend on authenticated state and add logout button

--- frontend/my-reservations-client/src/app/components/navheader/navheader.component.ts
+++ frontend/my-reservations-client/src/app/components/navheader/navheader.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthenticationService } from 'src/app/auth/authentication.service';
 
 @Component({
   selector: 'app-navheader',
   templateUrl: './navheader.component.html',
-  styleUrls: ['./navheader.component.scss']
+  styleUrls: ['./navheader.component.scss'],
 })
-export class NavheaderComponent implements OnInit {
+export class NavheaderComponent implements OnInit, OnDestroy {
+  authSub: Subscription;
+  isAuthenticated = false;
 
-  constructor() { }
+  constructor(private auth: AuthenticationService) {}
 
   ngOnInit() {
+    this.authSub = this.auth.authData.subscribe((data) => {
+      if (!data) {
+        this.isAuthenticated = false;
+      } else {
+        this.isAuthenticated = true;
+      }
+    });
   }
 
+  onLogout() {
+    console.log('Logout!');
+  }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
+  }
 }
