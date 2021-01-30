852e0dd63ff933d2450b4269c1a33a8bcbffe81a
Michal Rubajczyk <rubajticos@gmail.com>
2021-01-30 17:14:48 +0100

    Configure admin child routing

--- frontend/my-reservations-client/src/app/admin/admin-routing.module.ts
+++ frontend/my-reservations-client/src/app/admin/admin-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
+import { DoctorsComponent } from './doctors/doctors.component';
 
-const appRoutes: Routes = [{ path: '', component: AdminComponent }];
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [{ path: 'doctors', component: DoctorsComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(appRoutes)],
--- frontend/my-reservations-client/src/app/admin/admin.module.ts
+++ frontend/my-reservations-client/src/app/admin/admin.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { AdminComponent } from './admin/admin.component';
 import { AdminRoutingModule } from './admin-routing.module';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { DoctorsComponent } from './doctors/doctors.component';
 
 @NgModule({
-  declarations: [AdminComponent, HeaderComponent],
-  imports: [AdminRoutingModule],
+  declarations: [AdminComponent, HeaderComponent, DoctorsComponent],
+  imports: [AdminRoutingModule, RouterModule],
 })
 export class AdminModule {}
